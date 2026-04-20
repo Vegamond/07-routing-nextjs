7fb917a847b6bc2b458968839c5c422474fc57ce
Vega <alexkarpytskyi@gmail.com>
2026-04-20 03:16:59 +0300

    Fix @notes default import path

--- app/notes/filter/@notes/default.tsx
+++ app/notes/filter/@notes/default.tsx
@@ -1,5 +1,6 @@
-import NotesClient from '@/app/notes/Notes.client';
+import NotesClient from '@/app/notes/filter/[...slug]/Notes.client';
 
 export default function NotesDefault() {
-  return <NotesClient initialTag="all" />;
+  return <NotesClient initialTag='all' />;
 }
+
